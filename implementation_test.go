89802b30b146c6bac5a2ebcccf182148d8f987b8
DarthPigrum <darthpigrum@gmail.com>
2020-10-06 03:07:25 +0300

    More tests

--- implementation_test.go
+++ implementation_test.go
@@ -8,9 +8,29 @@ import (
 )
 
 func TestPostfixToInfix(t *testing.T) {
-	res, err := PostfixToInfix("1 22 + 333 * 4444 - 55555 / 6 ^")
+    res, err := PostfixToInfix("1")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "1", res)
+	}
+    res, err = PostfixToInfix("1 2 +")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "1+2", res)
+	}
+	res, err = PostfixToInfix("1 22 + 333 * 4444 - 55555 / 6 ^")
 	if assert.Nil(t, err) {
 		assert.Equal(t, "(((1+22)*333-4444)/55555)^6", res)
+	}
+	res, err = PostfixToInfix("1 2+	3 - 4 * 5 / 6 ^ 7 + 8 - 9 *10	/")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(((1+2-3)*4/5)^6+7-8)*9/10", res)
+	}
+	res, err = PostfixToInfix("1 +")
+	if !assert.NotNil(t, err) {
+		t.Fatal("Unary operator error not implemented")
+	}
+	res, err = PostfixToInfix("")
+	if !assert.NotNil(t, err) {
+		t.Fatal("Empty string error not implemented")
 	}
 }
 
